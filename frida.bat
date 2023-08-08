2dfc923d8b679772025e2f68fc964c4a679339e1
sim0n00ps <132307467+sim0n00ps@users.noreply.github.com>
2023-08-08 21:04:58 +0100

    Update frida.bat

--- frida.bat
+++ frida.bat
@@ -3,13 +3,10 @@
 set adb_path="YOUR PATH TO adb.exe HERE"
 set frida_server_name=YOUR FRIDA SERVER VERSION HERE
 
-rem Change the working directory to the platform-tools folder
 cd /d "YOUR platform-tools PATH HERE"
 
-rem Push the frida-server to /sdcard
 %adb_path% push %frida_server_name% /sdcard
 
-rem Start an adb shell and execute commands non-interactively
 echo su > commands.txt
 echo mv /sdcard/%frida_server_name% /data/local/tmp >> commands.txt
 echo chmod +x /data/local/tmp/%frida_server_name% >> commands.txt
@@ -17,5 +14,4 @@ echo /data/local/tmp/%frida_server_name% >> commands.txt
 %adb_path% shell < commands.txt
 del commands.txt
 
-rem Pause the script to keep the Command Prompt window open
 pause
