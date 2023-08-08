e91c59a2ec026108a6a5d98f7089e171be652d8f
sim0n00ps <132307467+sim0n00ps@users.noreply.github.com>
2023-08-08 21:04:28 +0100

    Update dumpkeys.bat

--- dumpkeys.bat
+++ dumpkeys.bat
@@ -1,16 +1,12 @@
 @echo off
 setlocal
 
-rem Set the path to the folder containing the Python script
 set "folder_path=YOUR PATH HERE"
 
-rem Set the name of the Python script to run
 set "python_script=dump_keys.py"
 
-rem Change the directory to the specified folder
 cd /d "%folder_path%"
 
-rem Run the Python script
 python "%python_script%"
 
 pause
